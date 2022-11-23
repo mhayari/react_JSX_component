e041fa25388e9d109fa94f03d096cc857f1c117f
Mohammed Hayari <mhayari@gmail.com>
2022-11-23 11:11:06 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-import ProfilPhoto from './Component/profile/ProfilPhoto';
-import FullName from './Component/profile/FullName';
-import Adress from './Component/profile/Adress';
+import Copy from './images/Copy.jpg'
+import ProfileComponent from './profile/ProfileComponent';
+
 
 function App() {
+  const handelName = (fullName) => {
+    alert(`${fullName}`)
+  }
   return (
-    <div className="App">
-      <ProfilPhoto />
-      <FullName />
-      <Adress/>
+    <div  className="App">
+      <ProfileComponent fullName='Mohammed Hayari' bio='im happy' profession='Developper' handelName={handelName} >
+        <img style={{width:'150px',heigth:'150px'}} src={Copy} alt='pic' />
+        </ProfileComponent>
     </div>
   );
 }
